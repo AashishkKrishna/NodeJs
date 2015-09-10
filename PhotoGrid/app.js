88e360979e06a6c313407a8223cac379f3a40d60
AashishkKrishna <aashish.k.krishna@hotmail.com>
2015-09-10 23:23:54 +0200

    Photogrid app: done for today i.e. 10/9/2015

--- PhotoGrid/app.js
+++ PhotoGrid/app.js
@@ -1,5 +1,28 @@
-var epxress = require('express'),
-	path = require('path')
+var express = require('express'),
+	path = require('path'),
+	config = require('./config/config.js'),
+	knox = require('knox')
 	
 var app = express();
-app.set('views')
+app.set('views',path.join(__dirname,'views'));
+app.engine('html',require('hogan-express'));
+app.set('view engine','html');
+
+app.use(express.static(path.join(__dirname,'')));
+app.set('port',process.env.PORT || 3000);
+app.set('host',config.host);
+
+var knoxClient = knox.createClient({
+	key : config.S3Accesskey,
+	secret: config.S3Secretkey ,
+	bucket : config.S3Bucket
+})
+
+require('./routes/routes.js')(express,app);
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+server.listen(app.get('port'),function(){
+	console.log("Photogrid running on port:- "+ app.get('port'));
+})
